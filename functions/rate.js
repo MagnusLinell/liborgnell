91fecc1587f4c15c84962343a800e546541699c0
Magnus Linell <magnus.linell@gmail.com>
2019-09-28 13:15:10 +0200

    updated function with db call

--- functions/rate.js
+++ functions/rate.js
@@ -1,7 +1,33 @@
-const ip = require('ip');
+
+const querystring = require('querystring');
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://liborgnell:LPkeEspM1PAiUkMZ@cluster0-93bme.mongodb.net/liborgnell?retryWrites=true&w=majority";
+const dbName = 'liborgnell';
+
+const unknownError = { statusCode: 500, body: JSON.stringify({ code: 500, error: { message: 'Unknown Error' } }) };
+const methodNotAllowedError = { statusCode: 405, body: JSON.stringify({ code: 405, error: { message: 'Method Not Allowed' } }) };
+
 exports.handler = async (event, context) => {
-    return {
-        statusCode: 200,
-        body: ip.address()
-    };
+    if (event.httpMethod !== "POST") {
+        return methodNotAllowedError;
+    }
+    const params = querystring.parse(event.body);
+    MongoClient.connect(uri, (err, client) => {
+        if (err) {
+            return unknownError;
+        }
+
+        const col = client.db(dbName).collection('rates');
+        col.insert([params], { w: 1 }, (err, result) => {
+            if (err) {
+                return unknownError;
+            }
+            client.close();
+
+            return {
+                statusCode: 200,
+                body: result
+            };
+        });
+    });
 };
