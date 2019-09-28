6b66affb6d9b37e36c4860fbaf8cfdbb0ffb02f0
Magnus Linell <magnus.linell@gmail.com>
2019-09-28 13:40:46 +0200

    add ip

--- functions/rate.js
+++ functions/rate.js
@@ -1,11 +1,12 @@
 
 const querystring = require('querystring');
+const ip = require('ip');
 const MongoClient = require('mongodb').MongoClient;
 const uri = "mongodb+srv://liborgnell:LPkeEspM1PAiUkMZ@cluster0-93bme.mongodb.net/liborgnell?retryWrites=true&w=majority";
 const dbName = 'liborgnell';
 
-const unknownError = (e) => ({ statusCode: 500, body: JSON.stringify({ code: 500, error: { message: 'Unknown Error', trace: e } }) });
-const methodNotAllowedError = { statusCode: 405, body: JSON.stringify({ code: 405, error: { message: 'Method Not Allowed' } }) };
+const unknownError = (e) => ({ ip: ip.address(), statusCode: 500, body: JSON.stringify({ code: 500, error: { message: 'Unknown Error', trace: e } }) });
+const methodNotAllowedError = { ip: ip.address(), statusCode: 405, body: JSON.stringify({ code: 405, error: { message: 'Method Not Allowed' } }) };
 
 exports.handler = async (event, context) => {
     if (event.httpMethod !== "POST") {
@@ -19,7 +20,8 @@ exports.handler = async (event, context) => {
         client.close();
         return {
             statusCode: 200,
-            body: result
+            body: result,
+            ip: ip.address(),
         };
     } catch (e) {
         return unknownError(e);
