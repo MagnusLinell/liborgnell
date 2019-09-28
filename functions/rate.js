6053fb5b6619cbaf158c738d3cdc3ca8f907ec25
Magnus Linell <magnus.linell@gmail.com>
2019-09-28 13:37:19 +0200

    update db connection

--- functions/rate.js
+++ functions/rate.js
@@ -4,30 +4,24 @@ const MongoClient = require('mongodb').MongoClient;
 const uri = "mongodb+srv://liborgnell:LPkeEspM1PAiUkMZ@cluster0-93bme.mongodb.net/liborgnell?retryWrites=true&w=majority";
 const dbName = 'liborgnell';
 
-const unknownError = { statusCode: 500, body: JSON.stringify({ code: 500, error: { message: 'Unknown Error' } }) };
+const unknownError = (e) => ({ statusCode: 500, body: JSON.stringify({ code: 500, error: { message: 'Unknown Error', trace: e } }) });
 const methodNotAllowedError = { statusCode: 405, body: JSON.stringify({ code: 405, error: { message: 'Method Not Allowed' } }) };
 
 exports.handler = async (event, context) => {
     if (event.httpMethod !== "POST") {
         return methodNotAllowedError;
     }
-    const params = querystring.parse(event.body);
-    MongoClient.connect(uri, (err, client) => {
-        if (err) {
-            return unknownError;
-        }
-
+    try {
+        const params = querystring.parse(event.body);
+        const client = await MongoClient.connect(uri);
         const col = client.db(dbName).collection('rates');
-        col.insert([params], { w: 1 }, (err, result) => {
-            if (err) {
-                return unknownError;
-            }
-            client.close();
-
-            return {
-                statusCode: 200,
-                body: result
-            };
-        });
-    });
-};
+        const result = await col.insert([params], { w: 1 });
+        client.close();
+        return {
+            statusCode: 200,
+            body: result
+        };
+    } catch (e) {
+        return unknownError(e);
+    }
+}
