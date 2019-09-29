53cfaf2e5a49aa45bf20542e2d6064e28d64528c
Magnus Linell <magnus.linell@gmail.com>
2019-09-29 16:48:34 +0200

    update

--- pages/rate.jsx
+++ pages/rate.jsx
@@ -9,7 +9,6 @@ const Rate = ({ query }) => {
     const [overall, setOverall] = useState(0);
     const onRate = async (e) => {
         e.preventDefault();
-        console.log({ overall });
         if (overall < 1 || overall > 10) {
             return;
         }
@@ -47,7 +46,7 @@ const Rate = ({ query }) => {
             <Main center>
                 <MaxWidth>
                     <form className={styles.form} onSubmit={onRate}>
-                        <h3>Betygsätt ölen {query.code}</h3>
+                        <h3>Betygsätt ölen {query.code} {query.beerId}</h3>
                         <label htmlFor="overall">Övergripande betyg (1-10)</label>
                         <input name="overall" type="number" min="1" max="10" onBlur={updateOverall} onChange={updateOverall} />
                         <button type="submit">Betygsätt</button>
