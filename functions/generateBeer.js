c9bcaf9096ec8b5edfd40ac3231d798e35f8a966
Magnus Linell <magnus.linell@gmail.com>
2019-09-29 17:10:22 +0200

    generate encoded uri

--- functions/generateBeer.js
+++ functions/generateBeer.js
@@ -10,7 +10,7 @@ exports.handler = async (event, context) => {
     try {
         const beerId = uuid();
         const code = uuid();
-        const toUrl = `https://www.liborgnell.com/rate?code=${encodeURI(code)}&beerId=${encodeURI(beerId)}`;
+        const toUrl = encodeURIComponent(`https://www.liborgnell.com/rate?code=${code}&beerId=${beerId}`);
         const qr = `${url}${toUrl}`;
         const client = await MongoClient.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
         const col = client.db(dbName).collection('beers');
